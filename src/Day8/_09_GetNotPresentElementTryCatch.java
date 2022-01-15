4b219396f2a8cc6b53a0c1cf39be2c8b3991f15a
hmk10 <halitmk@gmail.com>
2022-01-14 21:32:58 -0500

    Selenium - NoSuchElement Exception handled with Try-Catch

---
+++ src/Day8/_09_GetNotPresentElementTryCatch.java
@@ -0,0 +1,39 @@
+package Day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _09_GetNotPresentElementTryCatch {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+        WebElement startButton = driver.findElement(By.cssSelector("#start>button"));
+
+        startButton.click();
+
+        String text = "";
+
+        while (text.equals("")) {
+            try {
+                WebElement textElement = driver.findElement(By.cssSelector("#finish>h4"));
+                text = textElement.getText();
+            } catch (NoSuchElementException ex) {
+                System.out.println("Trying to locate element!");
+            }
+        }
+
+        System.out.println(text);
+
+        driver.quit();
+
+    }
+
+}
