8732493425f8a1ac3536f67ca5be93b4f8b5b291
hmk10 <halitmk@gmail.com>
2022-01-14 21:18:15 -0500

    Selenium - NoSuchElement Exception

---
+++ src/Day8/_07_GetNotPresentElement.java
@@ -0,0 +1,31 @@
+package Day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _07_GetNotPresentElement {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+        WebElement startButton = driver.findElement(By.cssSelector("#start>button"));
+
+        startButton.click();
+
+        // noSuchElement exception because element is not present in the website yet!!!!
+        // when you get noSuchElement -> 1-check locator 2-you might need wait
+        WebElement textElement = driver.findElement(By.cssSelector("#finish>h4"));
+        String text = textElement.getText();
+        System.out.println(text);
+
+        driver.quit();
+
+    }
+
+}
