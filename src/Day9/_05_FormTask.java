ff2f5211e128a2781b5e6efda60363af1b52117a
hmk10 <halitmk@gmail.com>
2022-01-17 20:28:38 -0500

    Selenium - FORM TASK

---
+++ src/Day9/_05_FormTask.java
@@ -0,0 +1,12 @@
+package Day9;
+
+public class _05_FormTask {
+
+    // ---    TASK    ---
+    // Navigate https://www.facebook.com/
+    // Click on Create New Account button
+    // Fill the form
+    // Get text for Sign Up button
+
+
+}
